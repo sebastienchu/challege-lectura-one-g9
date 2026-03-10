555ddddcb1d60a4193ee6ac69403f6df3c8d71ff
sebastian villarroel <sebastianalvarez12121@gmail.com>
2026-03-10 13:55:02 -0500

    Create PersonDTO.java

---
+++ src/main/java/com/alura/literatura/dto/PersonDTO.java
@@ -0,0 +1,35 @@
+package com.alura.literatura.dto;
+
+import lombok.Data;
+
+@Data
+public class PersonDTO {
+    private Integer birth_year;
+    private Integer death_year;
+
+    public Integer getBirth_year() {
+        return birth_year;
+    }
+
+    public void setBirth_year(Integer birth_year) {
+        this.birth_year = birth_year;
+    }
+
+    public Integer getDeath_year() {
+        return death_year;
+    }
+
+    public void setDeath_year(Integer death_year) {
+        this.death_year = death_year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
